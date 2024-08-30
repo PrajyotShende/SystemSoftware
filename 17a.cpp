23fc4a055d668a79aaea5d73b4eb68bb546398a8
PrajyotShende <shendeprajyot@gmail.com>
2024-08-30 23:11:00 +0530

    Hand ON List 1 Completed

--- 17a.cpp
+++ 17a.cpp
@@ -1,3 +1,15 @@
+/*
+============================================================================================
+Name: 17a.c
+Author: Prajyot Shende
+Description:  Write a program to simulate online ticket reservation. Implement write lock
+Write a program to open a file, store a ticket number and exit. Write a separate program, to
+open the file, implement write lock, read the ticket number, increment the number and print
+the new ticket number then close the file.
+Date: 28 Aug 2024
+============================================================================================
+*/
+
 #include <iostream>
 #include <fcntl.h>
 #include <unistd.h>
@@ -21,8 +33,18 @@ int main()
 	//cout<<c<<endl;
 
 	//cout<<readnum<<endl;
+	cout<<"File has read the number"<<endl;
 
 	close(a);
 
 	return 0;
 }
+
+/*
+================================================
+Output: ./a.out 
+	File has read the number 
+	
+
+================================================
+*/
--- 17b.cpp
+++ 17b.cpp
@@ -1,3 +1,15 @@
+/*
+============================================================================================
+Name: 17b.c
+Author: Prajyot Shende
+Description:  Write a program to simulate online ticket reservation. Implement write lock
+Write a program to open a file, store a ticket number and exit. Write a separate program, to
+open the file, implement write lock, read the ticket number, increment the number and print
+the new ticket number then close the file.
+Date: 28 Aug 2024
+============================================================================================
+*/
+
 #include <iostream>
 #include <fcntl.h>
 #include <unistd.h>
@@ -51,3 +63,23 @@ int main()
 	return 0;
 }
 
+
+/*
+================================================
+Output: 
+
+terminal 1
+./a.out
+Trying To Acquire Write Lock. Please stand by......
+Acquired Write Lock.
+Number of tickets sold = 0
+To buy a ticket, press enter
+Your Ticket no is 1
+
+
+terminal 2
+Trying To Acquire Write Lock. Please stand by......
+
+
+================================================
+*/
