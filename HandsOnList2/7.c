d242e8c815f5b304e57704e876a6ab01c46b5221
PrajyotShende <shendeprajyot@gmail.com>
2024-09-16 23:57:08 +0530

    HI

---
+++ HandsOnList2/7.c
@@ -0,0 +1,35 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void* print(void* a)
+{
+        printf("Thread %d is running has Thread ID :- %lu\n", *((int*)a),pthread_self());
+        //exit(1);
+}
+
+int main()
+{
+        pthread_t t[3];
+        int z[3];
+        int i;
+        for(i = 0; i<3; i++)
+        {
+                z[i] = i;
+                int b = pthread_create(&t[i], NULL, print, &z[i]);
+                if(b!=0)
+                {
+                        printf("Some error while creating thread %d\n", i);
+                        exit(0);
+                }
+        }
+
+        for(i = 0; i<3; i++)
+        {
+                pthread_join(t[i], NULL);
+        }
+
+        printf("Joining of threads completed\n");
+        return 0;
+}
+
